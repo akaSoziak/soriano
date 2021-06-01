34f5f719e2059e9f960fb8b3d0d938ee46b0d14f
akaSoziak <83789114+akaSoziak@users.noreply.github.com>
2021-06-01 02:26:12 +0200

    Update and rename script.js to bgParallax.js

---
+++ bgParallax.js
@@ -0,0 +1,36 @@
+(function(){
+
+    const propParallax = {
+
+        seccion: document.querySelector('.parallax'),
+        recorrido: null,
+        limite: null,
+    
+    
+    }
+    
+    
+    const metParallax = {
+    
+        inicio: function (){
+    
+            window.addEventListener('scroll', metParallax.scrollParallax)
+        },
+    
+        scrollParallax: function (){
+    
+            propParallax.recorrido = window.pageYOffset;
+            propParallax.limite = propParallax.seccion.offsetTop + propParallax.seccion.offsetHeight;
+    
+            if (propParallax.recorrido > propParallax.seccion.offsetTop - window.outerHeight && propParallax.recorrido <= propParallax.limite ) {
+    
+                propParallax.seccion.style.backgroundPositionY = (propParallax.recorrido - propParallax.seccion.offsetTop) / 1.5 + 'px'
+                
+            }
+    
+        }
+    }
+    
+    metParallax.inicio()
+
+}())
